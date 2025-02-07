0bc07575656ba7c8795e3737e943524b5d944417
Anurag <anuragsharma5617@gmail.com>
2025-02-07 07:44:57 +0530

    minor changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const balance = document.getElementById("balance");
+const moneyPlus = document.getElementById("money-plus");
+const moneyMinus = document.getElementById("money-minus");
+const list = document.getElementById("list");
+const form = document.getElementById("list");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
